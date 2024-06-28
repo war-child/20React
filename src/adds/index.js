68f6712f08f32db96d4759bd83bde59b0c4e06e2
warchild <wolf@lycanthropy.us>
2024-06-27 17:15:54 -0700

    app adjust

--- src/adds/index.js
+++ src/adds/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
-import App from './App';
+import './app.css';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
